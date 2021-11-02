d1c6d660153f84348641408b39491a50bc5efc99
Alastair Mucklow <amucklow@strangerpixel.com>
2021-11-02 10:37:54 +0000

    Add Easy Coding Standard

---
+++ ecs.php
@@ -0,0 +1,9 @@
+<?php
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(SetList::PSR_12);
+};
