9ea6cb2bedb3b0cc075abb37f861723ba53ce4d4
Fanis Strezos <Fanis.Strezos@dotmailer.com>
2022-08-04 16:09:45 +0000

    Merged PR 39079: Add Unit tests configurations and folder
    Add Unit tests configurations and folder
    
    Simple run
    - composer update
    - vendor/bin/phpunit to execute the sample test

---
+++ tests/ClientTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dotdigital\Tests;
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    //Todo: Write tests for client class
+    public function testSimple()
+    {
+        $this->assertTrue(true);
+    }
+}
