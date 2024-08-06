ef04f2b4f26171a76603123aa2a3c2751802da07
Alastair Mucklow <Alastair.Mucklow@dotmailer.com>
2024-08-06 14:51:22 +0000

    Merged PR 56881: Add setters for Channel Property models
    ## What's being changed
    
    The Channel Property models now have their own setters.
    
    ## Why it's being changed
    
    We have this code in our Magento integration:
    ```
    $emailChannelProperty = new EmailChannelProperty([
        'emailType' => 'html'
    ]);
    if ($optInType) {
        $emailChannelProperty->setOptInType($optInType);
    }
    $sdkContact->setChannelProperties([
        'email' => $emailChannelProperty
    ]);
    ```
    
    Previously we had only needed to hydrate a channel property using an array of all desired keys - and AbstractSingletonModel::hydrate() sets properties correctly. Before this change, though, specifically calling `setOptInType()` would call the magic `AbstractSingletonModel::__call` and return null.
    
    ## How to review / test this change
    
    - Put the above code into a local sandbox and run it
    - The final hydrated contact should have an email channel property with emailType and optInType set
    
    Related work items: #265934

--- src/V3/Models/Contact/ChannelProperties/EmailChannelProperty.php
+++ src/V3/Models/Contact/ChannelProperties/EmailChannelProperty.php
@@ -20,4 +20,37 @@ class EmailChannelProperty extends AbstractSingletonModel
      * @var string|null
      */
     protected ?string $optInType;
+
+    /**
+     * @param $status
+     *
+     * @return self
+     */
+    public function setStatus($status): EmailChannelProperty
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    /**
+     * @param $emailType
+     *
+     * @return self
+     */
+    public function setEmailType($emailType): EmailChannelProperty
+    {
+        $this->emailType = $emailType;
+        return $this;
+    }
+
+    /**
+     * @param $optInType
+     *
+     * @return self
+     */
+    public function setOptInType($optInType): EmailChannelProperty
+    {
+        $this->optInType = $optInType;
+        return $this;
+    }
 }
--- src/V3/Models/Contact/ChannelProperties/SmsChannelProperty.php
+++ src/V3/Models/Contact/ChannelProperties/SmsChannelProperty.php
@@ -20,4 +20,37 @@ class SmsChannelProperty extends AbstractSingletonModel
      * @var string|null
      */
     protected ?string $countryCode;
+
+    /**
+     * @param $optInType
+     *
+     * @return self
+     */
+    public function setOptInType($optInType): SmsChannelProperty
+    {
+        $this->optInType = $optInType;
+        return $this;
+    }
+
+    /**
+     * @param $status
+     *
+     * @return self
+     */
+    public function setStatus($status): SmsChannelProperty
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    /**
+     * @param $countryCode
+     *
+     * @return self
+     */
+    public function setCountryCode($countryCode): SmsChannelProperty
+    {
+        $this->countryCode = $countryCode;
+        return $this;
+    }
 }
